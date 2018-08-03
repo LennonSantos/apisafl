4606bffc95734bb010d161af21ba6539fa2ae208
Lennon <lennonbueno@sistemaathos.com.br>
2018-08-03 11:11:43 -0300

    ajustes

--- src/services/mysql/system.js
+++ src/services/mysql/system.js
@@ -5,15 +5,15 @@ const system = deps => {
       return new Promise((resolve, reject) => {
         const { connection, errorHandler } = deps
 
-        connection.query('select * from system order by id desc limit 1', [], (error, results) => {
+        connection.query('select title, version_android, version_ios from system order by id desc limit 1', [], (error, results) => {
           if (error) {
             errorHandler(error, `Falha ao buscar a versão do sistema.`, reject)
             return false
           }
 
-          const { title, version } = results[0]
+          const { title, version_android, version_ios } = results[0]
 
-          resolve({ title: title, version: version })
+          resolve({ title: title, version_android: version_android, version_ios: version_ios })
         })
       })
     }
